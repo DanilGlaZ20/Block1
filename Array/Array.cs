b6c89846e27de1981475a1f2186e8b05a747f53a
Данила Глазунов <glazdan17@gmail.com>
2022-04-23 00:30:21 +0300

    Different

---
+++ Array/Array.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Array
+{
+    class Array
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
